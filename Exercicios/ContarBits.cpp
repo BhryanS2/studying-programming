173ce7f461bbc11717ead0ce846f7d608ffd7fdf
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2020-12-21 18:47:15 -0300

    a 	modified:   ContarBits.cpp

--- Exercicios/ContarBits.cpp
+++ Exercicios/ContarBits.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
+    long long int n;
     cin >> n;
-    int cont = 1; 
+    long long int cont = 1; 
     
     while(1){
     	if(n % 2 == 1) cont++;
