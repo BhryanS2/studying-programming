f37ea15d6ba91180d6dba684554a8611e3338abb
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2020-12-21 19:01:09 -0300

    vamos de obi 	new file:   "\303\224nibus-OBI2017.cpp"

---
+++ Exercicios/Ônibus-OBI2017.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, A, B;
+    int p[10][10], q;
+
+    cin >> N >> A >> B;
+    
+    for(int i = 0; i < N-1; i++){
+        for(int j = 0; j < A; j++){
+        	cin >> p[i][j];
+		}
+    }
+    for(int i = 0; i < N-1; i++){
+        for(int j = 0; j < A; j++){
+        	cout << p[i][j] << " ";
+		}
+		cout << endl;
+    }
+}
+/*
+4 2 4
+1 2
+2 3
+3 4
+*/
