7571b52ea3d550c5e99c1bc94cd0acc973e374e7
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2020-12-31 19:23:51 -0300

    aceito

---
+++ Exercicios/CopaDoMundoOBI2010.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    queue <char> fila;
+
+    for(char c = 'A'; c <= 'P' ; c++) fila.push(c);
+
+    for(int i = 0; i < 15; i++){
+        int a, b, ca, cb;
+        
+        ca = fila.front();
+        fila.pop();
+        cb = fila.front();
+        fila.pop();
+
+        cin >> a >> b;
+
+        if(a>b) fila.push(ca);
+        else fila.push(cb);
+    }
+
+    cout << (char)fila.front() << endl;
+
+    return 0;
+}
