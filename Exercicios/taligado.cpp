6a1357ae62c811bd22f1f37a856181f0a47f4f70
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-02 15:23:33 -0300

    aceito

--- Exercicios/taligado.cpp
+++ Exercicios/taligado.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+#define MAXN 100001
+typedef vector<int> vi;
 
 int n, m;
+vi v[MAXN];
 
-int main(){
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  cin >> n >> m;
+  int t, a, b;
+
+  while(m--) {
+    cin >> t >> a >> b;
     
-    cin >> n >> m;
+    if (t) {
+      v[a].push_back(b);
+      v[b].push_back(a);
+    } else {
+      cout << (find(v[a].begin(), v[a].end(), b) != v[a].end()) << endl;
+    }
+  }
 }
