280a757bb0a82e2306f60ec86c59f93a06db9ec2
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-04 15:33:16 -0300

    aceito 	new file:   MelhorAluno.cpp     new file:   TelefoneP1.cpp

---
+++ Exercicios/MelhorAluno.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double pedro, paulo;
+    cin >> pedro >> paulo;
+    if(pedro <= paulo) cout << "Pedro" << endl;
+    else cout << "Paulo" << endl;
+}
