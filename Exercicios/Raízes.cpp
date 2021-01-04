4fe0c7853b8227d685cd9905d883517e94680114
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-04 14:33:02 -0300

    aceito

---
+++ Exercicios/Raízes.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+	double x;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> x;
+        cout << fixed << setprecision(4) << (sqrt(x)) << endl;
+    }
+}
