e1c2cb14dcc9b961b84dbc1d1799a716c7ae2798
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-08 13:52:39 -0300

    não acabado 	renamed:    "Tri\303\242ngulos.cpp" -> "Tri\303\242ngulosOBI2011.cpp"

--- Exercicios/TriângulosOBI2011.cpp
+++ Exercicios/TriângulosOBI2011.cpp
@@ -10,6 +10,10 @@ bool condicao(int a, int b, int c) {
     | b - c | < a < b + c
     | a - c | < b < a + c
     | a - b | < c < a + b
+    
+    a /\ b
+     /__\
+      c
     */
     int BmenosC = b - c, AmenosC = a - c, AmenosB = a - b;
 
@@ -25,10 +29,17 @@ bool condicao(int a, int b, int c) {
     } else return false;
 }
 
-bool pitagoras(int a, int b, int c) {
-    // a² = b² + c²
+char triangulos(int a, int b, int c){
+    /*
+    Se a² = b² + c²  =>  triângulo retângulo
+    Se a² < b² + c²  => triângulo acutângulo
+    Se a² > b² + c² => triângulo obtusângulo
+    */
     int res = b + c;
-    return a == res ? true : false;
+
+    if (a == res) return 'r';
+    if (a < res) return 'a';
+    if (a > res) return 'o';;
 }
 
 int main() {
@@ -36,20 +47,14 @@ int main() {
 
     cin >> a >> b >> c;
     bool veradeiroOuFalso = condicao(a, b, c);
-    bool res = pitagoras(pow(a, 2), pow(b, 2), pow(c, 2));
-
-    if (veradeiroOuFalso) {
-        if (a == b && b == c) {
-            cout << "a" << endl;
-        }
-        else { 
-            cout << (res ? "r" : "o") << endl;
-        }
-    }    
-else {
-        cout << "n" << endl;
-    }
-
-    //cout << endl << pow(a, 2) << " " << pow(b, 2) << " " << pow(c, 2) << " " << pow(b, 2) + pow(c, 2);
+    
+    /*
+    r = retângulo
+    a = acutângulo
+    o = obtusângulo
+    n = não é possivel fazer um triângulo
+    */
+    if (veradeiroOuFalso) cout << triangulos(pow(a, 2), pow(b, 2), pow(c, 2)) << endl;
+    else cout << "n" << endl;
 }
 
