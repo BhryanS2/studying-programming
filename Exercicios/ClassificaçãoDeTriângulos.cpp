1d8a0b1eaf984feb27b142293f223f4652e58cef
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-08 13:47:57 -0300

    aceito 	new file:   "Classifica\303\247\303\243oDeTri\303\242ngulos.cpp" 	deleted:    "Tri\303\242ngulos.cpp"

---
+++ Exercicios/ClassificaçãoDeTriângulos.cpp
@@ -0,0 +1,52 @@
+//https://neps.academy/br/exercise/312
+//Triângulos
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+bool condicao(int a, int b, int c) {
+    /*
+    1 2 1
+    | b - c | < a < b + c
+    | a - c | < b < a + c
+    | a - b | < c < a + b
+
+    a /\ b
+     /__\
+      c
+    */
+    int BmenosC = b - c, AmenosC = a - c, AmenosB = a - b;
+
+    BmenosC = BmenosC < 0 ? BmenosC * -1 : BmenosC;
+    AmenosC = AmenosC < 0 ? AmenosC * -1 : AmenosC;
+    AmenosB = AmenosB < 0 ? AmenosB * -1 : AmenosB;
+
+    if (BmenosC < a && a < b + c) {
+        if (AmenosB < c && c < a + b) {
+            if (AmenosC < b && b < a + c) return true;
+            else return false;
+        } else return false;
+    } else return false;
+}
+
+int triangulos(int a, int b, int c){
+    /*
+    1-Isósceles, 
+    2-Escaleno
+    3-Equilátero
+    */
+    if(a == b && b == c) return 3;
+    else if(a != b && b != c && a != c) return 2;
+    else return 1;
+}
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+    bool veradeiroOuFalso = condicao(a, b, c);
+    
+    if (veradeiroOuFalso) cout << "S" << endl << triangulos(a, b, c) << endl;
+    else cout << "N" << endl;
+}
+
