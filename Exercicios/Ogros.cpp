84ebfea35a59634c237bc21c80eb844977ccc539
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-09 13:40:29 -0300

    não acabado 	new file:   Ogros.cpp

---
+++ Exercicios/Ogros.cpp
@@ -0,0 +1,40 @@
+/*numero de premiação: 3, numero de ogros: 4
+faixa de premiação: 3, 5
+representando a premiação de cada faixa de premiação: 1 4 7
+representando qual força cada ogro: 2 3 9 4
+
+https://neps.academy/br/exercise/261
+Ogros
+Fonte: OBI 2008 - Primeira Fase
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> FaixaPreimo, RepresentacaoPremio, Ogros;
+
+int main() {
+    int n, m, x;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n - 1; i++){
+        cin >> x;
+        FaixaPreimo.push_back(x);
+    }
+
+    for (int i = 0; i < n; i++){
+        cin >> x;
+        RepresentacaoPremio.push_back(x);
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> x;
+        Ogros.push_back(x);
+    }
+
+    for(int i = 0; i < Ogros.size(); i++){
+        if(Ogros[i] >= FaixaPreimo[0]){
+            
+        }
+    }
+}
