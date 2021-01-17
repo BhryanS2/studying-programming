9cfe32e9475426d21741f38bd47dff7989d1aabb
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-17 11:32:11 -0300

    não acabado 	modified:   "Exercicios/Express\303\265es.cpp" 	new file:   Exercicios/Times.cpp

--- Exercicios/Expressões.cpp
+++ Exercicios/Expressões.cpp
@@ -1,37 +1,66 @@
 #include <iostream>
 #include <stack>
+#include <bits/stdc++.h>
+#include <algorithm>
 
 using namespace std;
-
+/*
+find(incio, fim, elemento que quiero pesquisar);
+*/
 int main(){
-    int n;
-	string S;
-	stack<char> pilha;
+    int n, cont = 0;
+	string s;
+	stack<char> pilha1, pilha2, pilha3;
 	bool ok = true;
 
-    cin >> n;
+    cin >> n;//([{}])
+
     
     for(int j = 0; j < n; j ++){
-    	
-        cin >> S;
-
-        for(int i = 0; i < S.size(); i++){
-        	
-            if(S[i] == '{' or S[i] == '[' or S[i] == '('){
-                pilha.push(S[i]);
-            }else if(S[i] == '}' or S[i] == ']' or S[i] == ')'){
-
-                if(pilha.empty()){
-                    ok = false;
-                    break;
-                }else{
-                    pilha.pop();
-                    ok = true;
+        getline(cin, s);
+        for(int i = 0; i < s.size(); i++){
+            cout << s[i] << endl;
+            if(s[i] == '{' or s[i] == '[' or s[i] == '('){
+                if(s[i] == '{') pilha1.push(s[i]);
+                else s[i] == '[' ? pilha2.push(s[i]) : pilha3.push(s[i]);
+            }else if(s[i] == '}' or s[i] == ']' or s[i] == ')'){
+                if(s[i] == '}'){
+                    if (pilha1.empty()) { 
+                        pilha1.pop();
+                        ok = false;
+                        break;
+                    } else {
+                        pilha1.pop();
+                    }
+                }else if(s[i] == ']'){
+                    if (pilha2.empty()) { 
+                        ok = false;
+                        break;
+                    } else {
+                        pilha2.pop();
+                    }
+                }else if(s[i] == ')') {
+                    if (pilha3.empty()) {
+                        ok = false;
+                        break;
+                    } else {
+                        pilha3.pop();
+                    }
                 }
             }
-            
         }
-
+        if(!pilha1.empty()){
+            pilha1.pop();
+            ok = false;
+        }
+        if(!pilha2.empty()){
+            pilha2.pop();
+            ok = false;
+        }
+        if (!pilha3.empty()) {
+            pilha3.pop();
+            ok = false;
+        }
         cout << (ok ? "S":"N") << endl;
     }
 	return 0;
@@ -45,4 +74,14 @@ int main(){
 (] S
 }{ N
 ([{}]) S
+****
+(((((((((({([])}])))))))))
+(] () () () () () () () () () [] () {}
+***
+4
+(]
+}{
+([{}])
+{}()[]
+
 */
---
+++ Exercicios/Times.cpp
@@ -0,0 +1,61 @@
+/*
+14 3
+felipe 4
+alvaro 8
+thiago 1
+rodrigo 3
+robson 2
+fabio 9
+ricardo 11
+rodolfo 0
+andre 14
+arthur 12
+ronaldo 55
+rogerio 30
+lucas 7
+rafael 17
+
+
+Time 1
+andre 14
+fabio 9
+felipe 4
+ronaldo 55 
+thiago 1
+14 + 9 + 4 + 55 + 1 = 28 + 55 = 83
+
+Time 2
+alvaro 8 
+arthur 12
+rodolfo 0
+rodrigo 3
+rogerio 30 
+8 + 12 + 0 + 3 + 30 = 11 + 12 + 30 = 23 + 30 = 53
+
+Time 3
+lucas 7 
+rafael 17
+ricardo 11
+robson 2
+7 + 17 + 11 + 2 = 37
+
+******************************************************
+4 3
+john 3
+richard 0
+greg 100
+rupert 20
+
+Time 1
+greg
+richard
+100
+
+Time 2
+rupert
+20
+
+Time 3
+john
+3
+*/
