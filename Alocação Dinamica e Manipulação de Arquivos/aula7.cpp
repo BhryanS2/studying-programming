6a88113c38ae1e5b71228a1034d4f3970a9f0faf
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-19 14:04:24 -0300

    escrevendo X no txt

---
+++ Alocação Dinamica e Manipulação de Arquivos/aula7.cpp
@@ -0,0 +1,36 @@
+/*
+Escrita em Arquivos
+    fputc:
+        - usada para escrever um carater no arquivo
+        int fputc (const char caractere, FILE * fluxo);
+        ex: fputc("x", arquivo);
+        se der tudo certo ela retorna um valor intero do caracter se ocorreu uma falha ela retorna "EOF" (-1)
+    fputs:
+        utiliada para escrever uma string no arquivo apartir de onde o ponteiro está apontando
+        int fputs (const char caractere, FILE * fluxo);
+        ex: fputs ("Neps", arquivo);
+        se der tudo certo ela retorna um valor não negativo, se ocorreu uma falha ela retorna "EOF" (-1)
+    fprintf:
+        utiliada para escrever uma string no arquivo apartir de onde o ponteiro está apontando
+        int fprintf ( FILE * fluxo, const char *formato, ...);
+        ex: fprintf (arquivo, "Nome = %s\n", "Neps");
+        se der tudo certo ela retorna um valor iqual a quantidade de caracteres exritos, se ocorreu uma falha ela retorna "EOF" (-1)
+*/
+
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    FILE *ponteiro;
+
+    ponteiro = fopen("arquivo.txt", "w");
+
+    if(ponteiro == NULL) cout << "arquivo nao foi aberto";
+    else {
+        cout << "arquivo aberto";
+        //escreve X no documento
+        fputc('X', ponteiro);
+        fclose( ponteiro );
+    }
+}
