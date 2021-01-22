5a62d8f12a9eb7f924091be550ff60d558dcba6c
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-01-22 18:13:36 -0300

    aceito

---
+++ Exercicios/Quermesse.cpp
@@ -0,0 +1,42 @@
+/*
+https://neps.academy/br/exercise/648
+Quermesse
+Fonte: OBI 2000 - Fase Única
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int teste = 1;
+    while (true) {
+        // lê número de convidados
+        int convidados;
+        cin >> convidados;
+        if (!convidados) break;
+
+
+        // lê entrada e encontra solução
+        int solucao = -1;
+        int posicao = 1;
+        while (posicao <= convidados) {
+            int ingresso;
+            cin >> ingresso;
+            if (posicao == ingresso) {
+                if (solucao != -1)
+                    cerr << "*** + de uma solucao" << endl;
+                else
+                    solucao = posicao;
+            }
+            posicao++;
+        }
+
+        // imprime solução
+        cout << "Teste " << teste << endl;
+        cout << solucao << endl;
+        cout << endl;
+
+        // vai para próximo teste
+        teste++;
+    }
+}
+
