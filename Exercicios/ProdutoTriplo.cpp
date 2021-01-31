fa2a5b4823420372b4e4bf8152bd4c7a07c89da2
root <root@DESKTOP-F874ECE.localdomain>
2021-01-31 13:44:20 -0300

    não acabado

--- Exercicios/ProdutoTriplo.cpp
+++ Exercicios/ProdutoTriplo.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    long long int n;
-    cin >> n;
-    long long int maior, medio, menor, mult;
-    int x;
-    vector <int> v;
-    for(int i = 0; i < n; i++){
-        cin >> x;
-        v.push_back(x);
-    }
-    cout << mult << endl;
+  long long int n;
+  cin >> n;
+  long long int maior, medio, menor, mult;
+  int x;
+  vector <int> v;
+  for (int i = 0; i < n; i++) {
+    cin >> x;
+    v.push_back(x);
+  }
+  cout << mult << endl;
 
 }
