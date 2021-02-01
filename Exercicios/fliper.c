eeb739dd75d2f5fd64843833b0fb1d395d482ba6
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-02-01 17:14:48 -0300

    success

--- Exercicios/fliper.c
+++ Exercicios/fliper.c
@@ -6,7 +6,9 @@ Fonte: OBI 2014 - Primeira Fase
 #include<bits/stdc++.h>
 int main() {
   int p, r;
+
   scanf("%d %d", &p, &r);
+
   if (p && r) printf("A");
   else if (p == 0) printf("C");
   else printf("B");
