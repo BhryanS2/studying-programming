e12074a0c0ab007f5c7eb0eb275647c4c6c2b545
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-02-08 14:37:07 -0300

    success

--- Exercicios/Ogros.cpp
+++ Exercicios/Ogros.cpp
@@ -1,44 +1,42 @@
-/*numero de premiação: 3, numero de ogros: 4
-faixa de premiação: 3, 5
-representando a premiação de cada faixa de premiação: 1 4 7
-representando qual força cada ogro: 2 3 9 4
-
+/*
 https://neps.academy/br/exercise/261
 Ogros
 Fonte: OBI 2008 - Primeira Fase
 */
-
 #include <bits/stdc++.h>
+#define MAX 100002
+
 using namespace std;
-vector<int> FaixaPreimo, RepresentacaoPremio, Ogros, FilaFinal;
+
+int n, m, a[MAX], b[MAX], p[MAX];
+
+int pontuacao(int forca) {
+  int ini = 0, fim = n;
+
+  while (fim - ini > 1) {
+    int med = (fim + ini) / 2;
+    if (a[med] <= forca) ini = med;
+    else fim = med;
+  }
+
+  return p[fim - 1];
+}
+
 
 int main() {
-    int n, m, x;
 
-    cin >> n >> m;
+  cin >> n >> m;
+  a[0] = 0;
+  for (int i = 0; i < n - 1; i++) cin >> a[i + 1];
+  for (int i = 0; i < n; i++) cin >> p[i];
+  for (int i = 0; i < m; i++) cin >> b[i];
 
-    for (int i = 0; i < n - 1; i++){
-        cin >> x;
-        FaixaPreimo.push_back(x);
-    }
+  bool first = true;
 
-    for (int i = 0; i < n; i++){
-        cin >> x;
-        RepresentacaoPremio.push_back(x);
-    }
-
-    for (int i = 0; i < m; i++) {
-        cin >> x;
-        Ogros.push_back(x);
-    }
-
-    for(int i = 0; i < Ogros.size(); i++){
-        if(Ogros[i] == FaixaPreimo[0]){
-            FilaFinal[i] = FaixaPreimo[0];
-        }
-    }
-
-    for(int i = 0; i < Ogros.size(); i++){
-        cout << FilaFinal[i] << " ";
-    }
-}
+  for (int i = 0; i < m; i++) {
+    if (first) first = false;
+    else cout << " ";
+    cout << pontuacao(b[i]);
+  }
+  cout << endl;
+};
