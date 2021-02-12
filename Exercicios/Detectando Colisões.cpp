f1e5eb1e9804817afbf56fe3f8ce082e00850070
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-02-12 12:18:36 -0300

    success

--- Exercicios/Detectando Colisões.cpp
+++ Exercicios/Detectando Colisões.cpp
@@ -1,51 +1,22 @@
 /*
 Detectando Colisões
-https://old.neps.academy/lesson/237
+https://neps.academy/br/exercise/310
+Fonte: OBI 2007 - Primeira Fase
 */
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int li;
-
-li x;
-vector <li> q1, q2;
-bool ok = false;
-int maiorq1, menorq1, maiorq2, menorq2;
-
 int main() {
-    for(int i = 0; i < 4; i++) {
-        cin >> x;
-        q1.push_back(x);
-    }
-    maiorq1 = x;
-    menorq1 = x;
-    for(int i = 0; i < 4; i++) {
-        cin >> x;
-        q2.push_back(x);
-    }
-    maiorq2 = x;
-    menorq2 = x;
+  int x[2][2], y[2][2];
 
-    for(int i = 0; i < 4; i++) {
-        if(q1[i] > maiorq1) maiorq1 = q1[i];
-        if(q1[i] < menorq1) menorq1 = q1[i];
-        if(q2[i] > maiorq2) maiorq2 = q2[i];
-        if(q2[i] < menorq2) menorq2 = q2[i];
-    }
-    /*
-    0 0 2 2
-    1 1 3 3	
-    menorq1 = 0;
-    maiorq1 = 2;
-
-    maiorq2 = 3;
-    menorq2 = 1;
-    */
-
-    for(int i = menorq1; i < maiorq2; i++) {
-        if(maiorq1 == i || menorq1 == i) ok = false;
-        else ok = true;
-    }
-
-    cout << (ok ? 0 : 1) << endl;
+  cin >> x[0][0] >> y[0][0] >> x[0][1] >> y[0][1];
+  cin >> x[1][0] >> y[1][0] >> x[1][1] >> y[1][1];
+  if (x[0][1] < x[1][0] || x[1][1] < x[0][0] || y[0][1] < y[1][0] || y[1][1] < y[0][0] || x[0][0] > x[1][1] || x[1][0] > x[0][1] || y[0][0] > y[1][1] || y[1][0] > y[0][1])
+    cout << "0\n";
+  else cout << "1\n";
+  return(0);
 }
+/*
+0 0 6 6
+3 3 12 12
+*/
