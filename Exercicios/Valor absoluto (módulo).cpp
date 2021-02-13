bb62c08ab4eebcb6b9ac0f02d959c56bf31144ca
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-02-13 15:35:16 -0300

    success

---
+++ Exercicios/Valor absoluto (módulo).cpp
@@ -0,0 +1,13 @@
+/*
+Valor absoluto (módulo)
+https://neps.academy/br/exercise/755
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+  cin >> n;
+  cout << abs(n) << endl;
+}
