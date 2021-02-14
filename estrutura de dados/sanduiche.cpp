8db15501fc14fcc6940b461cfb0388cea24460be
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-02-14 11:30:22 -0300

    success
    	modified:   ../estrutura de dados/sanduiche.cpp

--- estrutura de dados/sanduiche.cpp
+++ estrutura de dados/sanduiche.cpp
@@ -1,3 +1,8 @@
+/*
+https://neps.academy/br/course/10/lesson/276
+Fonte: OBI 2016 - Primeira Fase
+Sanduíche
+*/
 #include <bits/stdc++.h>
 using namespace std;
 const int MAX = 10e6 + 10;
@@ -11,11 +16,18 @@ int main() {
 
     for (int i = 0; i < n; v[n + i] = v[i], i++) cin >> v[i];
 
+    //for (int i = 0; i < n; v[n + i] = v[i], i++) cout << v[i] << "\n";
+
     int right = -1, cont = 0;
 
     for (int i = 0; i < n; i++) {
+        //printf("for\n\nright = %d, cont = %d, vetor[i] = %d \n", right, cont, v[i]);
+        while (right + 1 < i + n && cont < d) {
+            cont += v[++right];
+            //printf("while\n\nright = %d, cont = %d, vetor[right] = %d \n", right, cont, v[right]);
+        }
 
-        while (right + 1 < i + n && cont < d) cont += v[++right];
+        //printf("cont = %d,  d = %d\n", cont, d);
 
         if (cont == d) res++;
 
