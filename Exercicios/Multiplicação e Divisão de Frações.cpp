6bc15edf6801907fb07443ab044b9f027e8c99c5
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-02-15 07:34:51 -0300

    success

---
+++ Exercicios/Multiplicação e Divisão de Frações.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c, d;
+  int numerador, denominador;
+  char op;
+
+  cin >> a >> b >> c >> d >> op;
+
+  //cout << a << endl << b << endl << c << endl << d;
+
+
+  if (op == 'M') {
+    numerador = c * a;
+    denominador = d * b;
+  } else if (op == 'D') {
+    numerador = a * d;
+    denominador = b * c;
+  }
+
+  cout << numerador << " " << denominador;
+
+}
