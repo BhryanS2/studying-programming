c12fb6b51d72cbed366730b227a8d38bf3132825
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2021-05-29 10:38:58 -0300

    success

--- Exercicios/Entrega De Caixas OBI2020.cpp
+++ Exercicios/Entrega De Caixas OBI2020.cpp
@@ -8,11 +8,21 @@ Fonte: OBI 2020 - Primeira Fase - A
 using namespace std;
 
 int main() {
-  int x, y, z, cont = 0;
+  int x, y, z, cont = 3;
+  vector<int> v;
   cin >> x >> y >> z;
+  v.push_back(x);
+  v.push_back(y);
+  v.push_back(z);
+  sort(v.begin(), v.end());
+  x = v[0], y = v[1], z = v[2];
 
-  if (x + y < z) cout << 1 << endl;
-  else
-    if (x == y && x == z) cout << 3 << endl;
-    else cout << 2 << endl;
+  // se as caixas tiverem o mesmo tamanho, são três viagens
+  if (x == y && x == z) cout << 3 << endl;
+  // se couber duas caixas em uma são 1 viagem
+  else if (x + y < z || y + z < x || x + z < y) cout << 1 << endl;
+  // se couber uma caixa dentro da outra, só uma viagem
+  else if(x < y && y < z) cout << 1 << endl;
+  // se não for nem uma dessas, são duas viagens
+  else cout << 2 << endl;
 }
